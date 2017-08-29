dc0b8a14d0174f2304c0a57bd1458ffe705b0da8
bytesizelife <26351169+bytesizelife@users.noreply.github.com>
2017-08-29 22:12:46 +0200

    fix nettoyer la pile

--- 7e06/7e06test.asm
+++ 7e06/7e06test.asm
@@ -7,10 +7,13 @@ main:
   push dword 1337
   call estPair
   nop                           ; debug: check valeur retour dans eax
+  add esp, 4			; nettoyer argument (estPair ne le fait pas)
 
   push dword 42
   call estPair
   nop                           ; debug: check valeur retour dans eax
+  add esp, 4			; nettoyer argument (estPair ne le fait pas)
+;  mov esp, ebp			; variante
 
   mov  eax, 1                   ; exit
   mov  ebx, 0                   ; ok
